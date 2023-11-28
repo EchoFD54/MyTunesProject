7741bba0735f301dcca04e8503cf91c70f7522ea
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2023-11-28 12:32:17 +0100

    Folder play
    Can now play all of the songs in a folder, one after the other

--- src/MainWindowController.java
+++ src/MainWindowController.java
@@ -6,8 +6,9 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.util.Duration;
-
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainWindowController {
     @FXML
@@ -19,11 +20,20 @@ public class MainWindowController {
     @FXML
     private MediaView mediaView;
     private MediaPlayer mediaPlayer;
+    private List<Media> songList = new ArrayList<>();
+    private int songIndex = 0;
 
     public void initialize(){
-        String filePath = "C:\\Users\\aaron\\Music\\Mytunes\\Sparkle.mp3";
-        Media media = new Media(new File(filePath).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
+        File folder = new File("C:\\Users\\aaron\\Music\\Mytunes");
+        File[] songs = folder.listFiles(((dir, name) -> name.endsWith(".mp3")));
+
+        if(songs !=null){
+            for (File file:songs){
+                songList.add(new Media(file.toURI().toString()));
+            }
+        }
+        System.out.println("Number of songs in the playlist: " + songList.size());
+        mediaPlayer = new MediaPlayer(songList.get(songIndex));
         mediaPlayer.setAutoPlay(false);
         mediaView.setMediaPlayer(mediaPlayer);
 
@@ -43,11 +53,12 @@ public class MainWindowController {
                 playBtn.setText("Pause");
             }
         });
+        playNextSong();
 
         //Reset song
         mediaPlayer.setOnEndOfMedia(() -> {
-            mediaPlayer.seek(Duration.ZERO);
-            playBtn.setText("Play");
+            mediaPlayer.stop();
+            playNextSong();
     });
 
         //set the song progress
@@ -68,6 +79,45 @@ public class MainWindowController {
         });
 
 
+
+    }
+
+    private void playNextSong(){
+        if (songIndex < songList.size()) {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+            }
+
+            mediaPlayer = new MediaPlayer(songList.get(songIndex));
+            mediaPlayer.setAutoPlay(true);
+            mediaView.setMediaPlayer(mediaPlayer);
+
+            // Update the progress slider for the new song
+            songProgress.setValue(0);
+
+            // Set up the progress slider for the new song
+            songProgress.valueChangingProperty().addListener((observable, oldValue, isChanging) -> {
+                if (!isChanging) {
+                    double duration = mediaPlayer.getTotalDuration().toMillis();
+                    double currentTime = songProgress.getValue() * duration / 100.0;
+                    mediaPlayer.seek(new Duration(currentTime));
+                }
+            });
+
+            // Update the progress slider as the media plays
+            mediaPlayer.currentTimeProperty().addListener((observable, oldValue, newValue) -> {
+                if (!songProgress.isValueChanging()) {
+                    double progress = (newValue.toMillis() / mediaPlayer.getTotalDuration().toMillis()) * 100.0;
+                    songProgress.setValue(progress);
+                }
+            });
+
+            // Increment the index for the next song
+            songIndex++;
+        } else {
+            // All songs have been played, you can handle this scenario as needed
+            System.out.println("All songs have been played");
+        }
     }
 
 }
