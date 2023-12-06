68acc5e1e2b7cea0a24919cc0b5dc823c91831b4
Nelson  Veríssimo <nelsonverissimo@Nelsons-MacBook-Pro.local>
2023-12-06 10:21:35 +0100

    Added a ConnectionManager
    To connect to the Database

---
+++ src/dal/ConnectionManager.java
@@ -0,0 +1,25 @@
+package dal;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
+public class ConnectionManager {
+
+    private final SQLServerDataSource ds;
+
+    public ConnectionManager() {
+        ds = new SQLServerDataSource();
+        ds.setDatabaseName("CSe2023b_MyTunes_TeamF");
+        ds.setUser("CSe2023b_e_20");
+        ds.setPassword("CSe2023bE20#23");
+        ds.setServerName("EASV-DB4");
+        ds.setTrustServerCertificate(true);
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return ds.getConnection();
+    }
+
+}
