4b005612a11162870169190727de2d85c88f94d6
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2023-12-07 10:37:52 +0100

    Update MainWindowController.java

--- src/MainWindowController.java
+++ src/MainWindowController.java
@@ -248,11 +248,7 @@ public class MainWindowController {
             e.printStackTrace();
         }
     }
-
-    private Song createSongFromMediaFile(File file) {
-        return new Song(file.getName(), "Unknown Artist", "Unknown Genre", "0:00");
-    }
-
+    
     public void updateSongProperties(String title, String artist, String genre, String filePath) {
         // Create a new Media object from the selected file path
         Media newMedia = new Media(new File(filePath).toURI().toString());
